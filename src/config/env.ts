c618c0f0f2226efd04ab2833151bf25fed74562b
Jayce <datht@hivelab.com>
2026-03-11 15:10:23 +0700

    Update/githubaction

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,7 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: path.resolve(process.cwd(), '..env') });
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
 function requireEnv(key: string): string {
   const value = process.env[key];
